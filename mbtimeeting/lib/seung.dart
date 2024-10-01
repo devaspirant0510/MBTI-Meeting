5aab13098d68168311c86a863e2082bbd8df7030
sylee0915 <seungyup8@gmail.com>
2024-10-01 21:31:56 +0900

    테스트

---
+++ mbtimeeting/lib/seung.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Seung extends StatelessWidget {
+  const Seung({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+      //여기다가 작성
+      child: ,
+    );
+  }
+}
